ecaf3431a9ec681d03d1988eef956b6cd608770a
cloudartisan <david@cloudartisan.com>
2018-08-26 21:40:12 +1000

    Stubbed a setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# NOTE: Assumes setup on MacOS
+
+TF_VER=0.11.2
+
+#curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_SHA256SUMS
+#curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_SHA256SUMS.sig
+#curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_darwin_amd64.zip
+#gpg --keyserver keyserver.ubuntu.com --recv 348FFC4C
+#gpg --verify terraform_${TF_VER}_SHA256SUMS.sig terraform_${TF_VER}_SHA256SUMS
+#shasum -a 256 -c terraform_${TF_VER}_SHA256SUMS
