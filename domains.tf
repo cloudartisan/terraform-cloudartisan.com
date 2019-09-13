c3c1107e76beac3692e8750ecf035fcec5cbfe6b
cloudartisan <david@cloudartisan.com>
2019-09-13 16:21:37 +1000

    Pre-commit hooks and reformatting

--- domains.tf
+++ domains.tf
@@ -1,5 +1,5 @@
 resource "linode_domain" "cloudartisan_com" {
-	soa_email   = "hostmaster@cloudartisan.com"
+  soa_email   = "hostmaster@cloudartisan.com"
   ttl_sec     = "300"
   expire_sec  = "300"
   refresh_sec = "300"
@@ -9,26 +9,26 @@ resource "linode_domain" "cloudartisan_com" {
 
 # Use GMail for all @cloudartisan.com addresses
 variable "mx_records" {
-	type = "map"
-	default = {
-		"ASPMX.L.GOOGLE.COM"      = 10
-		"ALT1.ASPMX.L.GOOGLE.COM" = 20
-		"ALT2.ASPMX.L.GOOGLE.COM" = 20
-		"ASPMX2.GOOGLEMAIL.COM"   = 30
-		"ASPMX3.GOOGLEMAIL.COM"   = 30
-		"ASPMX4.GOOGLEMAIL.COM"   = 30
-		"ASPMX5.GOOGLEMAIL.COM"   = 30
-	}
+  type = "map"
+  default = {
+    "ASPMX.L.GOOGLE.COM"      = 10
+    "ALT1.ASPMX.L.GOOGLE.COM" = 20
+    "ALT2.ASPMX.L.GOOGLE.COM" = 20
+    "ASPMX2.GOOGLEMAIL.COM"   = 30
+    "ASPMX3.GOOGLEMAIL.COM"   = 30
+    "ASPMX4.GOOGLEMAIL.COM"   = 30
+    "ASPMX5.GOOGLEMAIL.COM"   = 30
+  }
 }
 
 resource "linode_domain_record" "MX_cloudartisan_com" {
-	count       = "${length(keys(var.mx_records))}"
-	domain_id   = "${linode_domain.cloudartisan_com.id}"
-	record_type = "MX"
-	# The subdomain to delegate to the target MX server
-  name        = "cloudartisan.com"
-  target      = "${element(keys(var.mx_records), count.index)}"
-  priority    = "${element(values(var.mx_records), count.index)}"
+  count       = "${length(keys(var.mx_records))}"
+  domain_id   = "${linode_domain.cloudartisan_com.id}"
+  record_type = "MX"
+  # The subdomain to delegate to the target MX server
+  name     = "cloudartisan.com"
+  target   = "${element(keys(var.mx_records), count.index)}"
+  priority = "${element(values(var.mx_records), count.index)}"
 }
 
 # GitHub Pages IP address 1
