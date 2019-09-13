8295d2fc04e5095e91aa371dafa64f5b386d881c
cloudartisan <david@cloudartisan.com>
2019-09-13 15:40:52 +1000

    Use tfswitch to select the Terraform version

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,11 +2,8 @@
 #
 # NOTE: Assumes setup on MacOS
 
-TF_VER=0.11.2
+brew unlink terraform
+brew install warrensbox/tap/tfswitch
+tfswitch
 
-curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_SHA256SUMS
-curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_SHA256SUMS.sig
-curl -O https://releases.hashicorp.com/terraform/${TF_VER}/terraform_${TF_VER}_darwin_amd64.zip
-gpg --keyserver keyserver.ubuntu.com --recv 348FFC4C
-gpg --verify terraform_${TF_VER}_SHA256SUMS.sig terraform_${TF_VER}_SHA256SUMS
-shasum --ignore-missing -a 256 -c terraform_${TF_VER}_SHA256SUMS
+exit 0
