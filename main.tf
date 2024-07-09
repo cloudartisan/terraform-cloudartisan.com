f9dfc932f950ca473cbb426c8d3861ff943a3267
cloudartisan <david@cloudartisan.com>
2024-07-09 12:22:10 +1000

    Adds Minecraft remote-exec

--- main.tf
+++ main.tf
@@ -13,6 +13,33 @@ provider "linode" {
 
 data "linode_profile" "me" {}
 
+resource "linode_instance" "cloudartisan_com" {
+  label  = "cloudartisan_com"
+  image  = "${var.image}"
+  kernel = "${var.kernel}"
+  region = "${var.region}"
+  type   = "${var.type}"
+
+  private_networking = false
+
+  ssh_key       = "${chomp(file(var.ssh_key_file))}"
+  root_password = "${random_string.password.result}"
+
+  # Install packages
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -q update",
+    ]
+  }
+
+  # Setup Minecraft
+  provisioner "remote-exec" {
+    inline = [
+      "wget -q http://git.io/Sxpr9g -O /tmp/msm && bash /tmp/msm"
+    ]
+  }
+}
+
 resource "random_string" "password" {
   length  = 32
   special = true
